ee62b0573a93c910af2c15eebe5fbbc35323f14d
Nat <nat@cmmc.io>
2018-01-13 13:27:33 +0700

    - add conditional classname. - remove unused.

--- src/App.js
+++ src/App.js
@@ -27,85 +27,20 @@ export default class App extends Component {
     this.storeData = store.state
 
     store.addListener(() => {
-      console.log('store has updated.', store)
-
       const storeDevices = this.storeData.devices
       const storeMessageArrived = {
         body: this.storeData.messageArrived
       }
-
 
       this.setState({
         devices: storeDevices,
         messageArrived: storeMessageArrived
       })
 
-
-      // const messageArrived = {
-      //   uuid: messageArrived.uuid,
-      //   time: moment.unix(messageArrived.gps_us).format('DD/MM/YYYY HH:mm:ss')
-      // }
-      //
-      // this.setState({
-      //   lastMessage: lastMessage,
-      //   topicMessageArrived: topicMessages
-      // })
-
-      // const devices = this.storeData.devices
-      // const devicesOnline = this.storeData.devicesOnline
-      //
-      // this.bufferDevices = this.state.bufferDevices
-      // const topicMessages = this.storeData.topicMessages
-      //
-      // const arrayDevices = []
-      // const arrayDevicesOnline = []
-      //
-      // if (devices !== this.state.devices) {
-      //   Object.keys(devices).map(key => {
-      //     arrayDevices.push(devices[key])
-      //   })
-      //   this.setState({devices: arrayDevices})
-      // }
-      //
-      // if (devicesOnline !== this.state.devicesOnline) {
-      //
-      //   let compareDevices = []
-      //
-      //   Object.keys(devicesOnline).map(key => {
-      //     arrayDevicesOnline[key] = (devicesOnline[key])
-      //     compareDevices.push(devicesOnline[key])
-      //   })
-      //   this.setState({devicesOnline: arrayDevicesOnline})
-      //
-      //   if (!_.isEmpty(compareDevices)) {
-      //     this.setState({devices: compareDevices})
-      //   }
-      //
-      //   console.log('devices : ', this.state.devices)
-      //   console.log('devices online : ', arrayDevicesOnline)
-      //   console.log('compare devices : ', compareDevices)
-      // }
-      //
-      // if (topicMessages !== this.state.topicMessages) {
-      //
-      //   const messageArrived = JSON.parse(topicMessages)
-      //
-      //   const lastMessage = {
-      //     uuid: messageArrived.uuid,
-      //     time: moment.unix(messageArrived.gps_us).format('DD/MM/YYYY HH:mm:ss')
-      //   }
-      //
-      //   this.setState({lastMessage: lastMessage})
-      //
-      //   this.setState({topicMessages: topicMessages})
-      // }
     })
-
-    console.log('constructor', this.props)
   }
 
   componentDidMount () {
-    console.log('componentDidMount', this.props)
   }
 
   render () {
@@ -116,13 +51,7 @@ export default class App extends Component {
         <Navbar/>
 
         <div className="row" style={{marginTop: 30}}>
-
-          {
-            this.state.devices.map(device => {
-              return <Device data={device} key={uuid()}/>
-            })
-          }
-
+          {this.state.devices.map(device => { return <Device data={device} key={uuid()}/> })}
         </div>
 
         <div className="row" style={{marginTop: 20, marginBottom: 30}}>
@@ -133,13 +62,6 @@ export default class App extends Component {
 
                 <div className="form-group">
                   <h3>Message Arrived</h3>
-                </div>
-
-                <div className="form-group text-primary">
-                  <b>
-                    {/*uuid : {this.state.messageArrived.uuid.substr(-4)}<br/>*/}
-                    {/*sent : {(this.state.lastMessage.time) ? moment(this.state.lastMessage.time).fromNow() : ''}*/}
-                  </b>
                 </div>
 
                 <div className="form-group">
--- src/components/Device.js
+++ src/components/Device.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import moment from 'moment-timezone'
+import classNames from 'classnames'
 
 moment.locale('th')
 
@@ -17,12 +18,7 @@ export default class Device extends Component {
       let dataShouldBeArrivedAtTime = this.state.data.server_unix + this.state.data.sleep_time_ms
       let remainingTime = dataShouldBeArrivedAtTime - moment.now()
 
-      // console.log('uuid : ', this.state.data.uuid)
-      // console.log('dataShouldBearrivedAtTime : ', dataShouldBeArrivedAtTime)
-      // console.log('remainingTime : ', remainingTime)
-
-      const d = Object.assign({}, this.state.data)
-      d.will_update_ms = (remainingTime/1000).toFixed(2)
+      const d = Object.assign({}, this.state.data, {will_update_ms: (remainingTime / 1000).toFixed(2)})
       this.setState({data: d})
     }, 100)
   }
@@ -46,6 +42,10 @@ export default class Device extends Component {
       }
     }
 
+    var txtClass = classNames({
+      'text': true,
+      'text-danger': this.state.data.will_update_ms < 60
+    })
     return (
       <div className="col-md-3">
         <div className="form-group">
@@ -53,34 +53,31 @@ export default class Device extends Component {
             <div className='card-header bg-success'>
               <b>
                 <small style={{color: 'white'}}>
-                  UUID : {this.state.data.uuid.substr(-4)}<br/>
+                  {/*UUID : {this.state.data.uuid.substr(-4)}<br/>*/}
                   LINKIT : {this.state.data.linkit}
                 </small>
               </b>
             </div>
             <div className="card-body text-primary">
               <b>
-                <p className='text-danger'>
+                <p className={txtClass}>
                   <i className='fa fa-clock-o'/>
                   &ensp;{moment(this.state.data.server_unix).fromNow()}
                 </p>
-                <p className='text-danger'>
-                  next update in:
-                  &ensp;{this.state.data.will_update_ms}s
-                </p>
-                <p style={styles.content}>working_count : {this.state.data.working_count}</p>
-                <p style={styles.content}>uptime_s : {this.state.data.uptime_s}</p>
-                <p style={styles.content}>battery_raw: {this.state.data.battery_raw}</p>
-                <p style={styles.content}>open_fail : {this.state.data.open_fail}</p>
-                <p style={styles.content}>boot_count : {this.state.data.boot_count}</p>
-                <p style={styles.content}>sent_data_count : {this.state.data.sent_data_count}</p>
-                <p style={styles.content}>gps_diff : {this.state.data.gps_diff}</p>
+                <p style={styles.content} className={txtClass}>Next
+                  update: <span>{this.state.data.will_update_ms}s</span></p>
+                <p style={styles.content}>Uptime : {this.state.data.uptime_s}s</p>
+                <p style={styles.content}>Battery: {this.state.data.battery_raw / 100.0}v</p>
+                {/*<p style={styles.content}>working_count : {this.state.data.working_count}</p>*/}
+                {/*<p style={styles.content}>open_fail : {this.state.data.open_fail}</p>*/}
+                {/*<p style={styles.content}>boot_count : {this.state.data.boot_count}</p>*/}
+                {/*<p style={styles.content}>sent_data_count : {this.state.data.sent_data_count}</p>*/}
                 <p
-                  style={styles.content}>{this.state.data.gps_latitude.toFixed(6)}, {this.state.data.gps_longitude.toFixed(6)}</p>
+                  style={styles.content}>{this.state.data.gps_latitude.toFixed(6)}, {this.state.data.gps_longitude.toFixed(6)}
+                  ({this.state.data.gps_diff})</p>
                 <hr/>
-                <p className='text-secondary' style={styles.footer}>parser_version
-                  : {this.state.data.parser_version}</p>
-                <p className='text-secondary'>app_version : {this.state.data.app_version}</p>
+                <p className='text-secondary' style={styles.footer}>App v{this.state.data.app_version}, Parser
+                  v{this.state.data.parser_version}</p>
               </b>
             </div>
           </div>
@@ -88,86 +85,5 @@ export default class Device extends Component {
       </div>
     )
   }
-
 }
 
-// import React, { Component } from 'react'
-// import moment from 'moment-timezone'
-// moment.locale('th')
-//
-// export default class Device extends Component {
-//
-//   constructor (props) {
-//     super();
-//
-//     this.state = { }
-//     this.state.data = props.data
-//     // this.state.data.title = "hello"
-//     console.log(this.state.data)
-//   }
-//
-//   componentWillReceiveProps (props) {
-//     // this.cardHeaderActive = (props.online || !props.retain) ? 'card-header bg-success' : 'card-header bg-secondary'
-//   }
-//   componentDidMount() {
-//       this.timer = setInterval( () => {
-//         let dataShouldBeArrivedAtTime = this.state.data.server_unix + this.state.data.sleep_time_ms
-//         let remaingTime = dataShouldBeArrivedAtTime - moment.now()
-//         const d = Object.assign({}, this.state.data)
-//         d.will_update_ms = remaingTime
-//
-//         this.setState({data: d})
-//
-//       }, 1000)
-//   }
-//
-//   componentWillUnmount() {
-//       clearInterval(this.timer)
-//   }
-//
-//   render() {
-//
-//     const styles = {
-//       content: {
-//         marginBottom: 5
-//       },
-//       footer: {
-//         marginBottom: 0
-//       }
-//     }
-//     return (
-//       <div className="col-md-3">
-//         <div className="form-group">
-//           <div className="card">
-//             <div className='card-header bg-success'>
-//               <b>
-//                 <small style={{color: 'white'}}>
-//                   UUID : {this.state.data.uuid.substr(-4) }<br/>
-//                   LINKIT : {this.state.data.linkit}
-//                 </small>
-//               </b>
-//             </div>
-//             <div className="card-body text-primary">
-//               <b>
-//                 <p className='text-danger'><i className='fa fa-clock-o'/>&ensp;{moment(this.state.data.gps_us).fromNow()}</p>
-//                 {/*<p className='text-danger'>update in : &ensp;{this.state.data.sleep_time_ms}</p>*/}
-//                 {/*<p className='text-danger'>sleep_time : &ensp;{this.state.data.sleep_time_ms}</p>*/}
-//                 {/*<p style={styles.content}>will Update in: {this.state.data.will_update_ms} ms</p>*/}
-//                 <p style={styles.content}>working_count : {this.state.data.working_count}</p>
-//                 <p style={styles.content}>uptime_s : {this.state.data.uptime_s}</p>
-//                 <p style={styles.content}>battery_percent : {this.state.data.battery_percent}</p>
-//                 <p style={styles.content}>open_fail : {this.state.data.open_fail}</p>
-//                 <p style={styles.content}>boot_count : {this.state.data.boot_count}</p>
-//                 <p style={styles.content}>sent_data_count : {this.state.data.sent_data_count}</p>
-//                 <p style={styles.content}>gps_diff : {this.state.data.gps_diff}</p>
-//                 <hr/>
-//                 <p className='text-secondary' style={styles.footer}>parser_version : {this.state.dataparser_version}</p>
-//                 <p className='text-secondary'>app_version : {this.state.data.app_version}</p>
-//               </b>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
